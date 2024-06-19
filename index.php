4c2710c9b0d51a36fcb34edcc7d45b95215a4ee1
toshamasuke <imani.masuke@gmail.com>
2024-06-19 09:50:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}else {echo("Successfully connected"."<br>\n");}
 
 
-$result = $conn->query("SELECT * from mymtnactivity where msisdn="".$_GET['msisdn']."");
-if ($result->num_rows > 0) { echo "found"; } else { echo "not found";}
-
 ?>
