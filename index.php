70c5be4950150678b4c15de2aadddc8a3281db51
toshamasuke <imani.masuke@gmail.com>
2024-06-19 09:05:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 
-echo("Results for ".$_GET['msisdn']."\n");
+echo("Results for ".$_GET['msisdn']."<br>\n");
 $servername = "servercheckmsisdn.mysql.database.azure.com";
 $username = "admincheckmsisdn";
 $password = "pass1MSISDN";
 $dbname = "checkmsisdn";
 
-echo("Starting Connection to DB...."."\n");
+echo("Starting Connection to DB...."."<br>\n");
 $conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}else {echo("Successfully connected"."\n");}
+if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}else {echo("Successfully connected"."<br>\n");}
 
 ?>
